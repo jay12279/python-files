2380c51d7b6be17dcd0bd07557d46a44569810ca
jay12279 <jayanth03456@gmail.com>
2025-09-19 02:48:11 -0400

    first commit

---
+++ practicecode.py
@@ -0,0 +1,14 @@
+name = input("Enter your First name : ")
+print("Hello",name)
+
+last= input("Enter your last name:")
+print(last)
+
+dob= input("Enter your DOB:")
+print(dob)
+
+number= input("Enter your phone number:")
+print(number)
+
+altn= input("Enter your alternative number:")
+print(altn)
