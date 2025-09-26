bf62f16e4a711093f43095f867b5b5ec00029d4a
jay12279 <jayanth03456@gmail.com>
2025-09-26 15:06:33 -0400

    Add files via upload

---
+++ swapping two numbers week-2.py
@@ -0,0 +1,10 @@
+#swapping two numbers
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+
+print("\nBefore swapping: a=", a, "b =",b)
+
+a, b = b, a
+
+print("After swapping: a=", a, "b =",b)
+        
