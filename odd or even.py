5cbbe2f3d0aa56001ec3ec008ef526a1c18da080
jay12279 <jayanth03456@gmail.com>
2025-10-13 19:36:34 -0400

    Delete odd or even.py

--- odd or even.py
+++
@@ -1,8 +0,0 @@
-#1.checking if the number is even or odd
-
-Number= int(input("Enter the Number:"))
-
-if Number %2==0: #%2 is the modulas operator remainder 
-    print("The Number is Even.") #if the remainder is 0 it is even 
-else:
-    print("The Number is Odd") #if the reminder is not 0 it is odd
