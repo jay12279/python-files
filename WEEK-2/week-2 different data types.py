e4db66d64ddaf1071f82b3e20c87d44d32dff7d7
jay12279 <jayanth03456@gmail.com>
2025-10-13 18:59:45 -0400

    Add files via upload

---
+++ WEEK-2/week-2 different data types.py
@@ -0,0 +1,48 @@
+#string data type Text
+name='jayanth'
+lastname='marapalli'
+greeting='how are you'
+
+print(name)
+print(lastname)
+print(greeting)
+
+#string concatenation
+full_greeting=greeting+" "+name+" "+lastname
+print("concatenation:",full_greeting)
+
+#integers data type Numbers
+x = 22
+y = 2
+
+print("Addition:", x+y)
+print("Subtraction:",x-y)
+print("Multiplication:",x*y)
+print("power:",x**y)
+print("division:",x/y)
+print("floor division:",x//y)
+print("modulus:",x%y)
+
+
+#floating point numbers decimal form
+x = 5.6
+y = 7.5
+
+print("Addition:",x+y)
+print("\nfloat divison:",x/y) #in python \n is known as normal division,\n refers to the output prints in  the new line 
+
+
+#boolean data types True/false
+
+is_student= True
+do_job= False
+
+print("Is he Student:",is_student)
+print("Does he do Job:",do_job)
+
+#comparisions gives true or false
+
+print("\ncomparisions:") #\n prints in the new line, comparisions refers weather its true or false
+print(10>5)
+print(10<5)
+
